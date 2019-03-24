ebedff66591a1b56ef149b171d06ddb3fc834b6a
ruanjiayou <1439120442@qq.com>
2019-03-24 23:49:56 +0700

    re init

---
+++ test/parse().js
@@ -0,0 +1,160 @@
+var Uri = require('../uri');
+var assert = require('assert');
+
+// 测试parse()函数
+describe('Uri.parse()', function () {
+    it('parse http', function () {
+        var uri_http = new Uri('http://ruanjiayou:123456@jiayou.com:8080/admin/index.html?time=3333&redirect=/#top');
+        assert.equal(uri_http.protocol, 'http:');
+        assert.equal(uri_http.username, 'ruanjiayou');
+        assert.equal(uri_http.password, '123456');
+        assert.equal(uri_http.hostname, 'jiayou.com');
+        assert.equal(uri_http.port, '8080');
+        assert.equal(uri_http.pathname, '/admin/index.html');
+        assert.equal(uri_http.hash, '#top');
+        assert.equal(uri_http.search, '?time=3333&redirect=/');
+    });
+    it('parse https', function () {
+        var uri_https = new Uri('https://ruanjiayou:123456@jiayou.com:8080/admin/index.html?time=3333&redirect=/#top');
+        assert.equal(uri_https.protocol, 'https:');
+        assert.equal(uri_https.username, 'ruanjiayou');
+        assert.equal(uri_https.password, '123456');
+        assert.equal(uri_https.hostname, 'jiayou.com');
+        assert.equal(uri_https.port, '8080');
+        assert.equal(uri_https.pathname, '/admin/index.html');
+        assert.equal(uri_https.hash, '#top');
+        assert.equal(uri_https.search, '?time=3333&redirect=/');
+    });
+    it('test querystring', function () {
+        var uri1 = new Uri('http://jiayou.com/?s=1&s=2');
+        var uri2 = new Uri('http://jiayou.com/?s[]=1&s[]=2');
+        var uri3 = new Uri('http://jiayou.com/?s[1]=1&s[2]=2');
+        var uri4 = new Uri('http://jiayou.com/?s[2]=1&s[1]=2');
+        var uri5 = new Uri('http://jiayou.com/?s[s1]=1&s[]=2');
+        var uri6 = new Uri('http://jiayou.com/?s[s1]=1&s[2]=2');
+        assert.deepEqual(uri1._search, { s: ['1', '2'] });
+        assert.deepEqual(uri2._search, { 's': ['1', '2'] });
+        assert.deepEqual(uri3._search, { 's': ['1', '2'] });
+        assert.deepEqual(uri4._search, { s: ['2', '1'] });
+        assert.deepEqual(uri5._search, { s: { s1: '1', '0': '2' } });
+        assert.deepEqual(uri6._search, { s: { s1: '1', '2': '2' } });
+
+        assert.equal(uri1.search, '?s[0]=1&s[1]=2');
+        assert.equal(uri2.search, '?s[0]=1&s[1]=2');
+        assert.equal(uri3.search, '?s[0]=1&s[1]=2');
+        assert.equal(uri4.search, '?s[0]=2&s[1]=1');
+        assert.equal(uri5.search, '?s[0]=2&s[s1]=1');
+        assert.equal(uri6.search, '?s[2]=2&s[s1]=1');
+    });
+    it('parse ftp', function () {
+        var uri_ftp = new Uri('ftp://ruanjiayou:123456@jiayou.com:8080/admin/index.html?time=3333&redirect=/#top');
+        assert.equal(uri_ftp.protocol, 'ftp:');
+        assert.equal(uri_ftp.username, 'ruanjiayou');
+        assert.equal(uri_ftp.password, '123456');
+        assert.equal(uri_ftp.hostname, 'jiayou.com');
+        assert.equal(uri_ftp.port, '8080');
+        assert.equal(uri_ftp.pathname, '/admin/index.html');
+        assert.equal(uri_ftp.hash, '#top');
+        assert.equal(uri_ftp.search, '?time=3333&redirect=/');
+    });
+    it('parse javascript', function () {
+        var uri_javascript = new Uri('javascript:void(0);');
+        assert.equal(uri_javascript.protocol, 'javascript:');
+        assert.equal(uri_javascript.other, 'void(0);');
+    });
+    it('parse mailto', function () {
+        var uri_mailto = new Uri('mailto:1439120442@jiayou.com');
+        assert.equal(uri_mailto.protocol, 'mailto:');
+        assert.equal(uri_mailto.username, '1439120442');
+        assert.equal(uri_mailto.hostname, 'jiayou.com');
+    });
+    it('parse tel', function () {
+        var uri_tel = new Uri('tel:18972376482');
+        assert.equal(uri_tel.protocol, 'tel:');
+        assert.equal(uri_tel.other, '18972376482');
+    });
+    it('parse escape http', function () {
+        var uri_escape = new Uri('http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F8f805d073b9fc28374d34c25adca5220.jpeg');
+        assert.equal(uri_escape.protocol, 'http:');
+        assert.equal(uri_escape.username, '');
+        assert.equal(uri_escape.password, '');
+        assert.equal(uri_escape.hostname, 'bos.nj.bpc.baidu.com');
+        assert.equal(uri_escape.port, '');
+        assert.equal(uri_escape.pathname, '/v1/mediaspot/8f805d073b9fc28374d34c25adca5220.jpeg');
+        assert.equal(uri_escape.search, '');
+        assert.equal(uri_escape.hash, '');
+        assert.equal(uri_escape.other, '');
+    });
+    it('parse 中文http', function () {
+        var uri_zhcn = new Uri('http://bos.nj.bpc.baidu.com/v1/[破晓电影]/8f805d073b9fc28374d34c25adca5220.jpeg');
+        assert.equal(uri_zhcn.protocol, 'http:');
+        assert.equal(uri_zhcn.username, '');
+        assert.equal(uri_zhcn.password, '');
+        assert.equal(uri_zhcn.hostname, 'bos.nj.bpc.baidu.com');
+        assert.equal(uri_zhcn.port, '');
+        assert.equal(uri_zhcn.pathname, '/v1/[破晓电影]/8f805d073b9fc28374d34c25adca5220.jpeg');
+        assert.equal(uri_zhcn.search, '');
+        assert.equal(uri_zhcn.hash, '');
+        assert.equal(uri_zhcn.other, '');
+    });
+    it('parse php特殊uri', function () {
+        var o = new Uri('http://php.blog.jiayou.com/index.php/index/article1/id/985.html');
+        assert.equal(o.protocol, 'http:');
+        assert.equal(o.username, '');
+        assert.equal(o.password, '');
+        assert.equal(o.hostname, 'php.blog.jiayou.com');
+        assert.equal(o.port, '');
+        assert.equal(o.pathname, '/index.php/index/article1/id/985.html');
+        assert.equal(o.search, '');
+        assert.equal(o.hash, '');
+        assert.equal(o.other, '');
+    });
+    it('parse http strict-pathname', function () {
+        var o1 = new Uri('http://www.jiayou.com');
+        var o2 = new Uri('http://www.jiayou.com/');
+        assert.equal(o1.pathname, '/');
+        assert.equal(o2.pathname, '/');
+    });
+    it('parse http strict-hash', function () {
+        var o1 = new Uri('http://www.jiayou.com/#top');
+        var o2 = new Uri('http://www.jiayou.com/##top');
+        assert.equal(o1.hash, '#top');
+        assert.equal(o2.hash, '##top');
+    });
+    it('parse //开头的', function () {
+        var o = new Uri('//upload-images.jianshu.io/upload_images/1908904-1cc78e4125891a10.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240');
+        assert.equal(o.protocol, 'http:');
+    });
+    it('parse magnet', function () {
+        var o = new Uri('magnet:?xt=urn:btih:AC7E66737FD707F26FA5619AC4F0CA2C591E85AB');
+        assert.equal(o.protocol, 'magnet:');
+        assert.equal(o.other, '?xt=urn:btih:AC7E66737FD707F26FA5619AC4F0CA2C591E85AB');
+    });
+    it('parse ip', function () {
+        var o = new Uri('http://192.168.1.11:80');
+        assert.equal(o.protocol, 'http:');
+        assert.equal(o.hostname, '192.168.1.11');
+        assert.equal(o.port, '80');
+    });
+    it('parse !origin', function () {
+        var o = new Uri('http://www.jiayou.com:9010/test.jpeg!origin');
+        assert.equal(o.pathname, '/test.jpeg!origin');
+    });
+    it('parse 前后有空格', function () {
+        var o = new Uri(' http://www.jiayou.com/test ');
+        assert.equal(o.protocol, 'http:');
+        assert.equal(o.pathname, '/test');
+    });
+    it('setter search', function () {
+        var o = new Uri('http://www.jiayou.com/test?limit=1&page=1');
+        o.search = {
+            md5: 1
+        };
+        assert.equal(o.search, '?md5=1');
+        o.search = {
+            md5: null,
+            page: undefined
+        };
+        assert.equal(o.search, '?md5=');
+    });
+});
---
+++ uri.js
@@ -0,0 +1,361 @@
+const qs = require('qs');
+const pathToRegexp = require('path-to-regexp');
+
+class Uri {
+    /**
+     * @constructor
+     * @param {string} str 
+     */
+    constructor(str) {
+        // 协议 http https ftp gopher file javascript malto tel other
+        this.protocol = '';
+        // 用户名
+        this.username = '';
+        // 密码
+        this.password = '';
+        // 主机名或IP
+        this.hostname = '';
+        // 端口 
+        this.port = '';
+        // 路径 file协议也是这个
+        this.pathname = '';
+        // 查询参数 ?开头
+        this._search = {};
+        // 锚点 #开头
+        this.hash = '';
+        // other 
+        this.other = '';
+        if (str) {
+            this.parse(str.trim());
+        }
+    }
+    /**
+     * 把字符串解析为URI对象。必须是绝对路径。
+     * @param {string} str 
+     * @return {object} - this
+     */
+    parse(str) {
+        var index = -1;
+        // 1.验证类型
+        if (typeof str !== 'string') {
+            throw (new Error('类型必须是string！'));
+        }
+        // 2.解码
+        if (/^http%3A%2F%2F/.test(str)) {
+            str = unescape(str);
+        }
+        // 3.protocol
+        var temp = /^(\w+)[:](.+)$/.exec(str);
+        if (temp) {
+            this.protocol = temp[1];
+            str = temp[2];
+        } else {
+            this.protocol = 'http:';
+        }
+        if (Uri.HREF.has(this.protocol) || this.protocol === 'mailto:') {
+            // 去掉开头的/ file:类型要保留第三个/
+            str = str.replace(/^[/]{0,2}/, '');
+            // 4.username password
+            temp = /^([a-z0-9]+)([:]([a-z0-9]+))?[@](.+)$/ig.exec(str);
+            if (temp) {
+                this.username = temp[1];
+                if (temp[3] !== undefined) {
+                    this.password = temp[3];
+                }
+                str = temp[4];
+            }
+            // 5.hostname port
+            temp = /^([^/:]+)([:](\d+))?(.*)/.exec(str);
+            if (temp) {
+                this.hostname = temp[1];
+                if (temp[3] !== undefined) {
+                    this.port = temp[3];
+                }
+                str = temp[4];
+            }
+            // 6.hash
+            index = str.indexOf('#');
+            if (index !== -1) {
+                this.hash = str.substring(index);
+                str = str.slice(0, index);
+            }
+            // 7.search
+            index = str.indexOf('?');
+            if (index !== -1) {
+                this.search = str.substr(index);
+                str = str.slice(0, index);
+            }
+            // 8.pathname
+            this.pathname = str;
+        } else {
+            this.other = str;
+        }
+        return this;
+    }
+    /**
+     * 格式化URI对象
+     * @param {object} json - 所有set字段
+     */
+    format(json) {
+        for (let k in json) {
+            switch (k) {
+                case 'protocol':
+                    this.protocol = json[k];
+                    break;
+                case 'username':
+                    this.username = json[k];
+                    break;
+                case 'password':
+                    this.password = json[k];
+                    break;
+                case 'hostname':
+                    this.hostname = json[k];
+                    break;
+                case 'port':
+                    this.port = json[k];
+                    break;
+                case 'pathname':
+                    this.pathname = json[k];
+                    break;
+                case 'search':
+                    this.search = json[k];
+                    break;
+                case 'hash':
+                    this.hash = json[k];
+                    break;
+                case 'other':
+                    this.other = json[k];
+                    break;
+                default: break;
+            }
+        }
+        return this;
+    }
+    /**
+     * 将URI对象转为字符串
+     * @return {string} 
+     */
+    toString() {
+        // url other 两种种类型
+        return this.protocol + (Uri.HREF.has(this.protocol) ? '//' : '') + this.userinfo + this.host + this.path + this.hash + this.other;
+    }
+    /**
+     * 依据当前URI对象，将字符串解析为URI对象并返回。可以是相对路径。
+     * @param {string} str 
+     * @return {object} 
+     */
+    create(str) {
+        var uri,
+            ps,
+            cs,
+            index,
+            path = '',
+            other = '';
+        str = str.trim();
+        if (/^(\w+)[:](.+)$/.test(str) || /^[/]{2}(.+)/.test(str)) {
+            return new Uri(str);
+        }
+        uri = new Uri().format({
+            'protocol': this.protocol,
+            'username': this.username,
+            'password': this.password,
+            'hostname': this.hostname,
+            'port': this.port
+        });
+        //分离出path
+        index = str.indexOf('?');
+        if (index !== -1) {
+            other = str.substring(index);
+            str = str.substring(0, index);
+        }
+        path = str;
+        // 分离search.hash
+        index = other.indexOf('#');
+        if (index !== -1) {
+            uri.hash = other.substring(index);
+            other = other.substring(0, index);
+        }
+        uri.search = other;
+        ps = path.charAt(0) === '/' ? [] : this.pathname.split('/');
+        cs = path.split('/');
+        // 路径比较处理 - 去掉最后的文件
+        if (ps.length > 0 && /.[.]./.test(ps[ps.length - 1])) {
+            ps.pop();
+        }
+        while (cs.length !== 0) {
+            if (cs[0] === '.') {
+                cs.shift();
+                break;
+            } else if (cs[0] === '..') {
+                cs.shift();
+                ps.pop();
+            } else {
+                break;
+            }
+        }
+        ps = ps.concat(cs);
+        for (var i = ps.length - 1; i > 0; i--) {
+            if (ps[i] === '') {
+                ps.splice(i, 1);
+            }
+        }
+        uri.pathname = ps.join('/');
+        return uri;
+    }
+    /**
+     * 根据指定URI对象，将对象本身转换为相对url路径。协议必须是http或https。
+     */
+    shortOf(parent) {
+        var ps = parent.pathname.split('/'),
+            cs = this.pathname.split('/'),
+            len = ps.length < cs.length ? cs.length : ps.length,
+            diffIndex = 0,
+            i = 0,
+            res = '';
+        if (ps.length > 0 && /.[.]./.test(ps[ps.length - 1])) {
+            ps.pop();
+        }
+        // compare directory
+        for (; i < len; i++) {
+            if (cs[i] !== ps[i]) {
+                diffIndex = i;
+                break;
+            }
+        }
+        ps.splice(0, diffIndex);
+        res += ps.map(function (item) { return '../'; }).join('');
+        res += cs.splice(diffIndex).join('/');
+        return res + this.search + this.hash;
+    }
+    /**
+     * 获取参数值
+     * @param {string} key - 参数名称
+     */
+    query(key) {
+        return this._search[key];
+    }
+    /**
+     * 返回url的拓展名(小写) 或 ''
+     */
+    ext() {
+        let m = /[.]([\w]+)$/.exec(this.pathname);
+        return m ? m[1].toLowerCase() : '';
+    }
+    /**
+     * 匹配指定模式路由,返回null或json
+     */
+    match(pat) {
+        let res = {};
+        let m = pat.re.exec(this.pathname);
+        if (m === null) {
+            return null;
+        }
+        pat.keys.forEach(function (item, index) {
+            res[item.name] = m[index + 1];
+        });
+        return res;
+    }
+    set protocol(str) {
+        this._protocol = /^([0-9a-z]+)[:]?$/i.test(str.toLowerCase()) ? RegExp.$1 + ':' : 'http:';
+    }
+    set username(str) {
+        this._username = str;
+    }
+    set password(str) {
+        this._password = str;
+    }
+    set hostname(str) {
+        this._hostname = str;
+    }
+    set port(str) {
+        if (/^\d*$/.test(str)) {
+            this._port = str;
+        }
+    }
+    set pathname(str) {
+        this._pathname = str.replace(/[?#]/g, function (match) {
+            return encodeURIComponent(match);
+        });
+    }
+    /**
+     * undefined会被删掉,null则转为空字符串
+     * @param {string|object} o - search值
+     */
+    set search(o) {
+        o = typeof o === 'object' ? o : (qs.parse(o.substr(0, 1) === '?' ? o.substr(1) : o));
+        this._search = o;
+    }
+    set hash(str) {
+        if (str && str.charAt(0) === '#') {
+            str = str.substring(1);
+        }
+        this._hash = str;
+    }
+    set other(str) {
+        this._other = str;
+    }
+
+    get href() {
+        return this.toString();
+    }
+    get origin() {
+        let res = this.protocol;
+        if (['ftp:', 'http:', 'https:'].indexOf(this.protocol) === -1) {
+            res += '//';
+        } else if ('file:' === this.protocol) {
+            res += '///';
+        }
+        res += this.host;
+        return res;
+    }
+    get host() {
+        return this.hostname + (this.port === '' ? '' : `:${this.port}`);
+    }
+    get userinfo() {
+        let res = this.username;
+        if (res) {
+            if (this.password) {
+                res += `:${this.password}`;
+            }
+            res += '@';
+        }
+        return res;
+    }
+    get auth() {
+        return this.userinfo;
+    }
+    get path() {
+        return this.pathname + this.search;
+    }
+    get protocol() {
+        return this._protocol;
+    }
+    get username() {
+        return this._username;
+    }
+    get password() {
+        return this._password;
+    }
+    get hostname() {
+        return this._hostname;
+    }
+    get port() {
+        return this._port;
+    }
+    get pathname() {
+        return Uri.HREF.has(this.protocol) && this._pathname.charAt(0) !== '/' ? `/${this._pathname}` : this._pathname;
+    }
+    get search() {
+        let res = qs.stringify(this._search);
+        return res === '' ? '' : `?${decodeURIComponent(res)}`;
+    }
+    get hash() {
+        return this._hash === '' ? '' : `#${encodeURI(this._hash)}`;
+    }
+    get other() {
+        return this._other;
+    }
+}
+Uri.HREF = new Set(['ftp:', 'http:', 'https:', 'file:']);
+
+module.exports = Uri;
